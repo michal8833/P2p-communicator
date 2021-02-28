ea68d0557664f52ae9a65d7328e28f82da83c4f4
Michal <michal.m8833@gmail.com>
2021-02-28 15:18:38 +0100

    remove unnecessary files

--- .env/run_base.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-DIR=$(dirname "$0")
-
-source $DIR/dockerhub.env
-
-$DIR/run.sh $TAG_BASE $CONTAINER_BASE
-
--- .env/run_ide.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-DIR=$(dirname "$0")
-APP=$1
-
-source $DIR/dockerhub.env
-
-$DIR/run.sh $TAG_IDE $CONTAINER_IDE $APP
-
